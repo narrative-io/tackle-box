6afb66d8e94ffe93344c9e252c11f4252faa1d8c
Chris Woodward <cwoodward@narrative.io>
2020-09-01 16:19:30 -0700

    Created button and tabs with dummy content

---
+++ src/components/button/Button.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+		I'm a button
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'nio-button'
+  }
+</script>
+
+<style></style>
---
+++ src/components/tabs/Tabs.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+		I'm tabs
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'nio-tabs'
+  }
+</script>
+
+<style></style>
