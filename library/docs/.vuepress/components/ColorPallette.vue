615ff27b4c59b5d5bee42577b9595fb2ec655146
Chris Woodward <cwoodward@narrative.io>
2020-09-06 17:58:51 -0700

    Added rest of primary colors and adjusted c-primary styles

--- library/docs/.vuepress/components/ColorPallette.vue
+++ library/docs/.vuepress/components/ColorPallette.vue
@@ -2,13 +2,19 @@
   <div class="color-pallette">
     <section class="primary-section">
       <h2>Primary Colors</h2>
+      <div class="color-instances primary-main">
+        <ColorInstance
+          :variableName="colors.primaryMain.variableName"
+          :colorValue="colors.primaryMain.colorValue"
+        ></ColorInstance>	
+      </div>
       <div class="color-instances">
         <ColorInstance
           v-for="color of colors.primary"
           :variableName="color.variableName"
           :colorValue="color.colorValue"
         ></ColorInstance>	
-      </div>
+      </div>  
     </section>
 
     <section class="secondary-section">
@@ -23,32 +29,37 @@
 
 <script>
 
-const colors = {
-  primary: [
-    {
-      variableName: '$nio-c-primary',
-      colorValue: '#0032e6'
-    },
-    {
-      variableName: '$nio-c-primary-dark',
-      colorValue: '#4B5FA7'
-    }
-  ],
-  secondary: [],
-  utility: []
-}
-
 export default {
   data: () => ({
     colors: {
+      primaryMain: {
+        variableName: '$nio-c-primary',
+        colorValue: '#0032e6'
+      },
       primary: [
         {
-          variableName: '$nio-c-primary',
-          colorValue: '#0032e6'
+          variableName: '$nio-c-primary-lightest',
+          colorValue: '#F2F4FE'
+        },
+        {
+          variableName: '$nio-c-primary-lighter',
+          colorValue: '#BDCAF9'
+        },
+        {
+          variableName: '$nio-c-primary-light',
+          colorValue: '#849BF4'
         },
         {
           variableName: '$nio-c-primary-dark',
           colorValue: '#4B5FA7'
+        },
+        {
+          variableName: '$nio-c-primary-darker',
+          colorValue: '#031553'
+        },
+        {
+          variableName: '$nio-c-primary-darkest',
+          colorValue: '#010A28'
         }
       ],
       secondary: [],
@@ -65,14 +76,15 @@ export default {
   .color-pallette
     section
       h2
-        
         margin: 30px 0px 20px 0px
       .color-instances
         display: grid
         grid-gap: 10px
-        grid-template-columns: repeat(auto-fill, minmax(300px, 1fr))
-      
-
+        margin-bottom: 10px
+        &:not(.primary-main)
+          grid-template-columns: repeat(auto-fill, minmax(300px, 1fr))
+        .color.primary-main
+          width: 100%
+        
       
 </style>
-
