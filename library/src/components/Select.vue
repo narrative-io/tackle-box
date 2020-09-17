2d7a304956dd601eadb67c22eebd2ec20dbcf6c2
Chris Woodward <cwoodward@narrative.io>
2020-09-17 13:07:09 -0700

    Fixed input spacing IAW figma, added template to icon slot for select

--- library/src/components/Select.vue
+++ library/src/components/Select.vue
@@ -6,7 +6,9 @@
       v-on="$listeners"
       @input="$emit('update', $event)"
     )
-      slot
+      template(v-slot:append)
+        svg(style="width:24px;height:24px" viewBox="0 0 24 24")
+          path(fill="#425290" d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z")
 </template>
 
 <script>
