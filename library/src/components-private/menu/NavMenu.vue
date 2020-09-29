2c410cf11435aca76fc4e5505ec721c9fd69b3c7
Chris Woodward <cwoodward@narrative.io>
2020-09-29 11:16:21 -0700

    Created private compoents/styles dirs and created nav-memnu component/mixin

---
+++ library/src/components-private/menu/NavMenu.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+    .menu Nav menu 
+</template>
+
+<script>
+  export default {
+    props: {
+      
+    },
+    data: () => ({
+      parsedRules: []
+    }),
+    methods: {
+      
+    },
+    mounted() {	
+      this.$emit('mounted')
+    },
+    destroyed() {
+      this.$emit('destroyed')
+    }
+  }
+</script>
+
+<style lang="sass" scoped>
+  @import '../styles/mixins/_nav-menu'  
+</style>
