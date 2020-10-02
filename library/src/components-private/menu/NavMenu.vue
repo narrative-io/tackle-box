4c10e46e74773a3795b37a1322c098406c8ee871
Chris Woodward <cwoodward@narrative.io>
2020-10-01 21:09:06 -0700

    Refactored styles, added content-class to menu, removed unnecessary container elements

--- library/src/components-private/menu/NavMenu.vue
+++ library/src/components-private/menu/NavMenu.vue
@@ -1,27 +1,27 @@
 <template lang="pug"> 
-  v-menu.nav-menu(
-    open-on-click 
-    offset-y 
-    v-model="showMenu" 
-    :close-on-content-click="false"
-  )
-    template(v-slot:activator="{ on }")
-      .activator(@click="open")
-        .h3.text-white.button-label-small {{ userInitials }}
-    div
-      v-card(style="position: relative")
-        UserMenu(
-          :developerName="developerName"
-          :appName="appName"
-          :companies="companies" 
-          :user="user ? user : null"
-          :navItems="navItems"
-          ref="nio-nav-menu-ref"
-          @navItemClicked="close" 
-          @close="close" 
-          @navEvent="fireNavEvent($event)"
-          keep-alive
-        )
+	.nio-nav-menu
+		v-menu(
+			open-on-click 
+			offset-y 
+			v-model="showMenu" 
+			content-class="nio-nav-menu"
+			:close-on-content-click="false"
+		)
+			template(v-slot:activator="{ on }")
+				.activator(@click="open")
+					.h3.text-white.button-label-small {{ userInitials }}
+			UserMenu(
+				:developerName="developerName"
+				:appName="appName"
+				:companies="companies" 
+				:user="user ? user : null"
+				:navItems="navItems"
+				ref="nio-nav-menu-ref"
+				@navItemClicked="close" 
+				@close="close" 
+				@navEvent="fireNavEvent($event)"
+				keep-alive
+			)
 </template>
 
 <script>
@@ -67,26 +67,4 @@ export default {
 
 <style lang="sass" scoped>
   @import '../../styles-private/_nav-menu'  
-  .activator
-    width: 2rem
-    height: 2rem
-    margin-left: 1.25rem
-    padding: 0.3125rem
-    padding: 0rem
-    position: relative
-    transition: opacity 0.1s ease
-    border-radius: 0.25rem  
-    background-color: $c-tangerine-dark
-    +nio-center-content  
-    .h3
-      letter-spacing: 0rem
-    &:hover
-      cursor: pointer
-      opacity: 0.8
-  .v-card
-    background-color: $c-canvas !important
-    width: 23.75rem
-    padding: 32px 24px    
-    box-shadow: 0 5px 5px -3px rgba(0,0,0,.2), 0 8px 10px 1px rgba(0,0,0,.14), 0 3px 14px 2px rgba(0,0,0,.12) !important
-    border-radius: 1rem !important
 </style>
