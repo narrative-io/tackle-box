62728770224e2dd9fa154d64300701bec8de7a27
Chris Woodward <cwoodward@narrative.io>
2020-10-06 10:17:25 -0700

    Finished placeholder image and added optional headerImage prop to SideNav

--- library/src/components-private/SideNav.vue
+++ library/src/components-private/SideNav.vue
@@ -2,13 +2,14 @@
   .nio-side-nav
     .header
       ImageTile.avatar(
-        src="https://picffsum.photos/96/96"
+        :src="headerImage"
         size="large"
       )
         template(v-slot:nio-image-placeholder)
-          .image-placeholder HELLO
-            .top
-            .bottom
+          .image-placeholder
+            .content
+              .top
+              .bottom
       .app-info
         h4.text-primary-darker {{ appName }} 
         .p-small.text-primary-dark By <span class="nio-bold">{{ developerName }}</span>
@@ -33,7 +34,8 @@ export default {
     "appName": { type: String, required: true },
     "navItems": { type: Array, required: false, default: []},
     "hideItems": { type: Object, required: false, default: {} },
-    "lockItems": { type: Object, required: false, default: {} }
+    "lockItems": { type: Object, required: false, default: {} },
+    "headerImage": { type: String, required: false }
   },
   data: () => ({
     
@@ -58,4 +60,33 @@ export default {
 
 <style lang="sass" scoped>
   @import "../styles-private/_side-nav"
+
+  .image-placeholder
+    background-color: $c-primary
+    width: 100%
+    height: 100%
+    padding: 20px
+    .content
+      position: relative
+      overflow: hidden
+      width: 100%
+      height: 100%
+      .top
+        position: absolute
+        left: 0%
+        top: 0%
+        width: 100%
+        height: 50%
+        border-bottom-left-radius: 28px
+        border-bottom-right-radius: 28px
+        background: $c-white
+      .bottom
+        position: absolute
+        left: 0%
+        bottom: 0%
+        width: 100%
+        height: 50%
+        border-top-left-radius: 28px
+        border-top-right-radius: 28px
+        background: $c-primary-lighter
 </style>
