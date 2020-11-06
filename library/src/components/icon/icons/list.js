57643d992b531c03bc73dd0f111fe1e617c76ace
Chris Woodward <cwoodward@narrative.io>
2020-11-05 18:26:30 -0800

    Replaced display-list icon with Figma version, adjust stroke width

--- library/src/components/icon/icons/list.js
+++ library/src/components/icon/icons/list.js
@@ -1,43 +1,51 @@
-const IconList = ['task-list-to-do', 24, 24, [{
+const IconList = ['task-list-approve', 24, 24, [{
 	fill: 'none',
 	stroke: '#000',
 	'stroke-linecap': 'round',
-	'stroke-linejoin': 'round'
+	'stroke-linejoin': 'round',
+	'stroke-width': '1.0'
 }, {
 	fill: 'none',
 	stroke: '#000',
 	'stroke-linecap': 'round',
-	'stroke-linejoin': 'round'
+	'stroke-linejoin': 'round',
+	'stroke-width': '1.0'
 }, {
 	fill: 'none',
 	stroke: '#000',
 	'stroke-linecap': 'round',
-	'stroke-linejoin': 'round'
+	'stroke-linejoin': 'round',
+	'stroke-width': '1.0'
 }, {
 	fill: 'none',
 	stroke: '#000',
 	'stroke-linecap': 'round',
-	'stroke-linejoin': 'round'
+	'stroke-linejoin': 'round',
+	'stroke-width': '1.0'
 }, {
 	fill: 'none',
 	stroke: '#000',
 	'stroke-linecap': 'round',
-	'stroke-linejoin': 'round'
+	'stroke-linejoin': 'round',
+	'stroke-width': '1.0'
 }, {
 	fill: 'none',
 	stroke: '#000',
 	'stroke-linecap': 'round',
-	'stroke-linejoin': 'round'
+	'stroke-linejoin': 'round',
+	'stroke-width': '1.0'
 }, {
 	fill: 'none',
 	stroke: '#000',
 	'stroke-linecap': 'round',
-	'stroke-linejoin': 'round'
+	'stroke-linejoin': 'round',
+	'stroke-width': '1.0'
 }, {
 	fill: 'none',
 	stroke: '#000',
 	'stroke-linecap': 'round',
-	'stroke-linejoin': 'round'
-}], ['M5.5,20.5h9.08a2,2,0,0,0,1.519-.7l1.919-2.239a2,2,0,0,0,.482-1.3V5.5H5.5Z', 'M21.5,3.5a1,1,0,0,0-1-1H15.052a3.329,3.329,0,0,0-6.1,0H3.5a1,1,0,0,0-1,1v19a1,1,0,0,0,1,1h17a1,1,0,0,0,1-1Z', 'M15.5 8.5L11 8.5', 'M8.5 8.5L9 8.5', 'M15.5 12.5L11 12.5', 'M8.5 12.5L9 12.5', 'M14 16.5L11 16.5', 'M8.5 16.5L9 16.5']]
+	'stroke-linejoin': 'round',
+	'stroke-width': '1.0'
+}], ['M11.25 14.261L16.5 14.261', 'M11.25 18.761L16.5 18.761', 'M8.25,13.886a.375.375,0,1,1-.375.375.375.375,0,0,1,.375-.375', 'M11.25 9.761L16.5 9.761', 'M8.25,9.386a.375.375,0,1,1-.375.375.375.375,0,0,1,.375-.375', 'M8.25,18.386a.375.375,0,1,1-.375.375.375.375,0,0,1,.375-.375', 'M15.75,4.511H19.5a1.5,1.5,0,0,1,1.5,1.5v15.75a1.5,1.5,0,0,1-1.5,1.5H4.5a1.5,1.5,0,0,1-1.5-1.5V6.011a1.5,1.5,0,0,1,1.5-1.5H8.25a3.75,3.75,0,0,1,7.5,0Z', 'M12,3.761a.375.375,0,1,1-.375.375A.375.375,0,0,1,12,3.761']]
 
 export { IconList }
