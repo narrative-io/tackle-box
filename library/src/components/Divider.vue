474a737352fe5ece255dae798b8b68cf2f070099
Chris Woodward <cwoodward@narrative.io>
2020-11-17 15:10:14 -0800

    Finished NioDivider and test component

--- library/src/components/Divider.vue
+++ library/src/components/Divider.vue
@@ -1,17 +1,57 @@
 <template lang="pug">
   .nio-divider(
-
-  ) DIVIDER
+    :class="{'nio-horizontal': horizontal, 'nio-vertical': vertical, 'nio-emphasis': emphasis, 'nio-segmented': segmented}"
+  )
+    .segments(
+      v-if="segmented"
+    )
+      .start
+      .text.nio-h6.text-primary-dark OR
+      .end
 </template>
 
 <script>
   export default {
     name: 'nio-divider',
+    data: () => ({
+      segmented: false,
+      horizontal: null,
+      vertical: null,
+      emphasis: false
+    }),
     mounted() {	
+      this.applyHelperAttributes()
       this.$emit('mounted')
     },
     destroyed() {
       this.$emit('destroyed')
+    },
+    methods: {
+      applyHelperAttributes() {
+        const attributes = this.$el.attributes
+        if (attributes.getNamedItem('horizontal-solo')) {
+          this.horizontal = true     
+        }
+        if (attributes.getNamedItem('vertical-solo')) {
+          this.vertical = true
+        }
+        if (attributes.getNamedItem('horizontal-solo-emphasis')) {
+          this.horizontal = true
+          this.emphasis = true
+        }
+        if (attributes.getNamedItem('vertical-solo-emphasis')) {
+          this.vertical = true
+          this.emphasis = true
+        }
+        if (attributes.getNamedItem('horizontal-or')) {
+          this.horizontal = true
+          this.segmented = true
+        } 
+        if (attributes.getNamedItem('vertical-or')) {
+          this.vertical = true
+          this.segmented = true
+        }
+      }   
     }
   }
 </script>
--- library/src/test-components/TestDivider.vue
+++ library/src/test-components/TestDivider.vue
@@ -1,6 +1,29 @@
 <template lang="pug">
   .test-divider
-    NioDivider
+    .horizontal
+      img(src="https://picsum.photos/200/50")
+      NioDivider(horizontal-solo)
+      img(src="https://picsum.photos/200/50")
+    .vertical
+      img(src="https://picsum.photos/200/50")
+      NioDivider(vertical-solo)
+      img(src="https://picsum.photos/200/50")  
+    .horizontal
+      img(src="https://picsum.photos/200/50")
+      NioDivider(horizontal-solo-emphasis)
+      img(src="https://picsum.photos/200/50")
+    .vertical
+      img(src="https://picsum.photos/200/50")
+      NioDivider(vertical-solo-emphasis)
+      img(src="https://picsum.photos/200/50")    
+    .horizontal
+      img(src="https://picsum.photos/200/50")
+      NioDivider(horizontal-or)
+      img(src="https://picsum.photos/200/50")
+    .vertical
+      img(src="https://picsum.photos/200/50")
+      NioDivider(vertical-or)
+      img(src="https://picsum.photos/200/50")      
 </template>
 
 <script>
@@ -19,5 +42,26 @@ export default {
 
 <style lang="sass" scoped>
 .test-divider
-  
+  display: flex
+  flex-direction: column
+  align-items: center
+  .horizontal
+    width: 300px
+    display: flex
+    flex-direction: column
+    align-items: center
+    img
+      width: 200px
+      margin-bottom: 10px
+      margin-top: 10px
+  .vertical
+    width: 150px
+    height: 300px
+    display: flex
+    align-items: center
+    img
+      width: 50px
+      height: 200px
+      margin-left: 10px
+      margin-right: 10px    
 </style>
