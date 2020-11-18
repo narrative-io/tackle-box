2b5c6f5fd44f30cbc80eb7bb6461d2e7da0b229e
Chris Woodward <cwoodward@narrative.io>
2020-11-18 10:39:43 -0800

    Created two commononly used link slat types

---
+++ library/src/components/slat/assembled/IconFramerLinkSlat.vue
@@ -0,0 +1,37 @@
+<template lang="pug">
+  NioSlatLink.nio-icon-framer-link-slat
+		ImageTitleSubtitle(
+			:iconName="iconName"
+		)
+			template(v-slot:title)
+				slot(name="title")
+			template(v-slot:subtitle)
+				slot(name="subtitle")
+</template>
+
+<script>
+
+import NioSlatLink from '../types/SlatLink'
+
+export default {
+  name: 'icon-name-description-offset',
+  props: {
+		"iconName": { type: String, required: false }
+  },
+  data: () => ({
+
+  }),
+  mounted() {
+  },
+  methods: {
+    click() {
+      this.$emit('click')
+    }
+  },
+  components: { NioSlat, NioIcon }
+}
+</script>
+
+<style lang="sass" scoped>
+
+</style>
