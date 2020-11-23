908701d83369dd2808efcc304c93ac4423f9ebd8
Chris Woodward <cwoodward@narrative.io>
2020-11-23 10:57:25 -0800

    Added props and attributes

--- library/src/components/table/SlatTable.vue
+++ library/src/components/table/SlatTable.vue
@@ -8,14 +8,27 @@
 export default {
   name: 'nio-slat-table',
   props: {
+    "items": { type: Array, required: true },
+    "filters": { type: Array, required: false, default: []},
+    "action": { type: String, required: false, default: "menu"}, // menu | link | expand
+    "selection": { type: String, required: false, default: "none"}, // none | single | multiple
   },
   data: () => ({
-
+    searchable: false,
+    multiple: false
   }),
   mounted() {
   },
   methods: {
-    
+    applyHelperMethods() {
+      const attributes = this.$el.attributes
+      if (attributes.getNamedItem('paginate')) {
+        this.paginate = true
+      }
+      if (attributes.getNamedItem('filter')) {
+        this.filter = true
+      }
+    }
   },
   components: { }
 }
