36b608f7c020cb8d8eb86c077be816f60f3fa57b
Chris Woodward <cwoodward@narrative.io>
2020-12-03 14:32:36 -0800

    Replaced nav item status with new NioPill for reusablility

---
+++ library/src/components/Pill.vue
@@ -0,0 +1,29 @@
+<template lang="pug">
+  .nio-pill(:class="text") 
+    .content-wrapper
+      .content.text-c-white {{ text }}
+</template>
+
+<script>
+ 
+  export default {
+    name: 'nio-pill',
+    data: () => ({
+     
+    }),
+    props: {
+      "text": { type: String, required: true },
+      "backgroundColor": { type: String, required: false }
+    },
+    mounted() {	
+      this.$emit('mounted')
+    },
+    destroyed() {
+      this.$emit('destroyed')
+    }
+  }
+</script>
+
+<style lang="sass" scoped>
+  @import '../styles/mixins/_pill'  
+</style>
