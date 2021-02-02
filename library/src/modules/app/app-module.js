d54aaf29910a27469e5a7fb888007b51391ab346
Chris Woodward <cwoodward@narrative.io>
2021-02-02 09:20:21 -0800

    Added message handler

--- library/src/modules/app/app-module.js
+++ library/src/modules/app/app-module.js
@@ -1,4 +1,5 @@
 import heightObserver from './height-observer'
+import messageHandler from './message-handler'
 
 export default {
 	methods: {
@@ -11,9 +12,21 @@ export default {
 			console.log("remove")
 			console.log(elementName)
 			heightObserver.removeTrackedElement(elementName)
+		},
+		nioHandleMessage (evt) {
+			messageHandler.handleMessage(evt)
 		}
+	},
+	mounted() {
+		if (window.addEventListener) {
+      window.addEventListener("message", this.handleMessage, false);
+    }
+    else {
+      window.attachEvent("onmessage", this.handleMessage);
+    }
 	}
 }
+
 
 
 
