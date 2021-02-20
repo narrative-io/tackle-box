60d488a70d199944580e1ac1133d5d54de4456b4
Chris Woodward <cwoodward@narrative.io>
2021-02-20 11:12:32 -0800

    Added text field and linked to slider model

--- library/src/components/Slider.vue
+++ library/src/components/Slider.vue
@@ -1,14 +1,36 @@
 <template lang="pug">
-  v-slider.nio-slider(
-    v-bind="$attrs"
-    v-on="$listeners" 
-    :value="model"
-    @input="$emit('update', $event)"
-  )
+	.nio-slider
+		NioTextField.input-max(
+			v-if="rangeAttr"
+			hide-details
+			small
+			solo
+			type="number"
+			prepend="utility-dollar-sign"
+			v-model="model"
+			:value="model"
+		)
+		v-slider(
+			v-bind="$attrs"
+			v-on="$listeners" 
+			:value="model"
+			@input="$emit('update', $event)"
+		)
+		NioTextField.input-max(
+			hide-details
+			small
+			solo
+			type="number"
+			prepend="utility-dollar-sign"
+			v-model="model"
+			:value="model"
+		)
+
 </template>
 
 <script>
 
+import NioTextField from './TextField'
 
 export default {
   name: 'nio-slider',
@@ -24,7 +46,8 @@ export default {
     if (!this.value) {
       this.$emit('update', this.model)
     }
-  }
+	},
+	components: { NioTextField }
 }
 </script>
 
