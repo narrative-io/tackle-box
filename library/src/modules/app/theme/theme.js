947b1b78270943d1b96971fe714923e11b25c7db
Chris Woodward <cwoodward@narrative.io>
2021-05-26 11:40:40 -0700

    Move helper to approprate file

--- library/src/modules/app/theme/theme.js
+++ library/src/modules/app/theme/theme.js
@@ -1,16 +1,10 @@
 import { setGlobalStyles } from './global'
 import { GlobalColors } from './global'
+import { toKebabCase } from '../../helpers'
 
 function setTheme() {
 	let styles = document.documentElement.style
 	setGlobalStyles(styles)
-}
-
-function toKebabCase(string) {
-	return string && string
-    .match(/[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+/g)
-    .map(x => x.toLowerCase())
-    .join('-')
 }
 
 function getThemeColor(colorName) {
