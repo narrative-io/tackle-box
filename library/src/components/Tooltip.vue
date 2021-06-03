981491a4b7c66260d807dc6de6e5390695f8a009
Chris Woodward <cwoodward@narrative.io>
2021-06-03 16:27:33 -0700

    Created NioTooltip

---
+++ library/src/components/Tooltip.vue
@@ -0,0 +1,31 @@
+<template lang="pug">
+  .nio-tooptip
+
+</template>
+
+<script>
+
+import NioIcon from './icon/Icon'
+
+export default {
+  name: 'nio-tooltip',
+  props: {
+    
+  },
+  data() {
+    return {
+    }
+  },		
+  mounted() {
+    this.applyHelperAttributes()
+  },
+  methods: {
+    
+  },
+  components: { NioIcon }
+}
+</script>
+
+<style lang="sass" scoped>
+  @import '../styles/mixins/_tooltip'  
+</style>
