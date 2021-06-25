7c2daad23510ad58b9b175d75aa729096f1cf29b
Chris Woodward <cwoodward@narrative.io>
2021-06-24 18:34:49 -0700

    Revert "Revert "Refactored slider""
    This reverts commit eadea24cb5af19d331b1f434131a26e1402d0aeb.

--- library/src/test-components/TestSlider.vue
+++ library/src/test-components/TestSlider.vue
@@ -4,44 +4,61 @@
       v-model="model1"
       :min="10"
       :max="100"
-      :value="15"
       prepend
       currency
     ) 
     NioSlider(
-      v-model="model1"
+      v-model="model1Step"
       :min="10"
       :max="100"
-      :value="15"
+      :step="0.01"
+      prepend
+      currency
+    ) 
+    NioSlider(
+      v-model="model2"
+      :min="10"
+      :max="100"
       prepend
     ) 
     NioSlider(
-      v-model="model1"
-      :min="1"
+      v-model="model2Step"
+      :min="10"
       :max="100"
-      :value="15"
-    ) 
-    .test {{ model1 }}
-    NioSlider(
-      v-model="model2"
-      :min="1"
-      :max="100"
-    ) 
-    .test {{ model2 }}
-    NioSlider(
-      range
-      v-model="rangeModel"
-      :min="1"
-      :max="100"
+      :step="0.01"
+      prepend
     ) 
     NioSlider(
       range
       currency
-      v-model="rangeModel"
-      :min="1"
+      v-model="rangeModel1"
+      :min="1.00"
+      :max="10.00"
+      :step="0.01"
+    )
+    NioSlider(
+      range
+      currency
+      v-model="rangeModel2"
+      :min="0.05"
+      :max="4.00"
+      :step="0.05"
+    )
+    NioSlider(
+      range
+      currency
+      v-model="rangeModel3"
+      :min="10"
       :max="100"
     )
-    .test {{ rangeModel }}
+    NioSlider(
+      range
+      currency
+      v-model="rangeModel4"
+      :min="10"
+      :max="100"
+      :step="0.01"
+    )
 </template>
 
 <script>
@@ -55,12 +72,19 @@ export default {
   data: () => ({
     model1: 15,
     model2: 15,
-    rangeModel: [20, 40]
+    model1Step: 25,
+    model2Step: 25,
+    rangeModel1: [2.00, 4.00],
+    rangeModel2: [1.00, 1.50],
+    rangeModel3: [20, 40],
+    rangeModel4: [20, 40]
   })
 };
 </script>
 
 <style lang="sass" scoped>
 .test-slider
+  .nio-slider
+    margin-bottom: 100px
   
 </style>
