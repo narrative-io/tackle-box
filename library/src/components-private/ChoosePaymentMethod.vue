89c2e5321288050e2c05f18359cbf735e063b7d7
Chris Woodward <cwoodward@narrative.io>
2021-09-10 10:42:17 -0700

    Created ChoosePaymentMethod, test component and added to exports

---
+++ library/src/components-private/ChoosePaymentMethod.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+   .nio-choose-payment-method
+
+</template>
+
+<script>
+
+export default {
+  name: 'nio-choose-payment-method',
+  mounted() {	
+    this.$emit('mounted')
+  },
+  destroyed() {
+    this.$emit('destroyed')
+  },
+  components: { }
+}
+</script>
+
+<style lang="sass" scoped>
+  @import '../styles-private/_choose-payment-method'  
+</style>
+
---
+++ library/src/test-components/TestChoosePaymentMethod.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+  .test-choose-payment-method
+    
+</template>
+
+<script>
+
+import NioChoosePaymentMethod from '../components-private/ChoosePaymentMethod'
+
+export default {
+  components: {
+    NioChoosePaymentMethod
+  },
+  data: () => ({
+    
+  })
+};
+</script>
+
+<style lang="sass" scoped>
+.test-choose-payment-method
+</style>
