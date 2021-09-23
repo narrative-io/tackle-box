021aee45b722592a29fd9d3f045c00556f9abefc
Chris Woodward <cwoodward@narrative.io>
2021-09-23 15:41:28 -0700

    Created content and styles for component and test component

--- library/src/components/OverflowMenu.vue
+++ library/src/components/OverflowMenu.vue
@@ -1,24 +1,39 @@
 <template lang="pug">
-  .nio-overflow-menu Overflow Menu
+  .nio-overflow-menu
+    v-menu(
+      contentClass="nio-slat-table-item-menu"
+      left
+      nudgeBottom="20"
+    )
+      template(v-slot:activator="{ on, attrs }")
+        .activator(v-on="on")
+          NioIcon(
+            name="utility-more"
+            color="#415298"
+          )        
+      slot(name="items")
 </template>
 
 <script>
- 
-  export default {
-    name: 'nio-overflow-menu',
-    data: () => ({
-    
-    }),
-    props: {
-     
-    },
-    mounted() {	
-      this.$emit('mounted')
-    },
-    destroyed() {
-      this.$emit('destroyed')
-    }
-  }
+
+import NioIcon from './icon/Icon'
+
+export default {
+  name: 'nio-overflow-menu',
+  data: () => ({
+  
+  }),
+  props: {
+    data: { type: Object, required: false }
+  },
+  mounted() {	
+    this.$emit('mounted')
+  },
+  destroyed() {
+    this.$emit('destroyed')
+  },
+  components: { NioIcon }
+}
 </script>
 
 <style lang="sass" scoped>
--- library/src/test-components/TestOverflowMenu.vue
+++ library/src/test-components/TestOverflowMenu.vue
@@ -1,6 +1,17 @@
 <template lang="pug">
   .test-overflow-menu
-    NioOverflowMenu
+    .item(
+      v-for="item of items"
+    )
+      .nio-p.text-primary-darker {{ item.name }}
+      NioOverflowMenu(
+        :data="item"
+      )
+        template(v-slot:items)  
+          v-list
+            v-list-item Update budget for item # {{ item.id }}
+            v-list-item View item {{ item.id }}
+            v-list-item Visit item # {{ item.id }}  
 </template>
 
 <script>
@@ -12,12 +23,28 @@ export default {
     NioOverflowMenu
   },
   data: () => ({
-   
+    items: [
+      {
+        id: 1,
+        name: 'Item 1'
+      },
+      {
+        id: 2,
+        name: 'Item 2'
+      }
+    ]
   })
 };
 </script>
 
 <style lang="sass" scoped>
+
+@import '../styles/global/_colors'
+
 .test-overflow-menu
- 
+  .item
+    display: flex
+    border-bottom: 1px solid $c-primary-lighter
+    padding: 16px 24px
+    justify-content: space-between
 </style>
