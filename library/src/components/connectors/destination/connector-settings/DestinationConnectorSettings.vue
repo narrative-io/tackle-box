eb964a7d11262e5717ad9d8382bd678600e3a8c7
Chris Woodward <cwoodward@narrative.io>
2021-10-30 09:22:42 -0700

    Added name/description fields common to all destination connectors

--- library/src/components/connectors/destination/connector-settings/DestinationConnectorSettings.vue
+++ library/src/components/connectors/destination/connector-settings/DestinationConnectorSettings.vue
@@ -1,10 +1,32 @@
 <template lang="pug">
-  div.nio-destination-connector-settings DESTINATION CONNECTOR SETTINGS
-  
+  .nio-destination-connector-settings
+    .sub-container
+      .name.filter
+        .title-description
+          .filter-title.nio-h4.text-primary-darker Profile Name
+          .description.nio-p.text-primary-dark Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Vestibulum id ligula porta felis euismod semper.
+        .filter-value
+          NioTextField(
+            label="Profile Name"
+            v-model="model.name"
+            @update="updateModel"
+          )
+      .description.filter
+        .title-description
+          .filter-title.nio-h4.text-primary-darker Description
+          .description.nio-p.text-primary-dark Maecenas faucibus mollis interdum.
+        .filter-value
+          NioTextField(
+            label="Profile Description"
+            v-model="model.description"
+            @update="updateModel"
+          )
+
 </template>
 
 <script>
 
+import NioTextField from '../../../TextField'
 
 export default {
   name: 'nio-destination-connector-settings',
@@ -12,6 +34,10 @@ export default {
     
   },
   data: () => ({
+    model: {
+      name: null,
+      description: null
+    }
   }),
   computed: {
     
@@ -20,12 +46,14 @@ export default {
     
   },
   methods: {
-   
+    updateModel() {
+      this.$emit('detailsChanged', this.model)
+    }
   }, 
   destroyed() {
     this.$emit('destroyed')
   },
-  components: {  }
+  components: { NioTextField }
 }
 </script>
 
