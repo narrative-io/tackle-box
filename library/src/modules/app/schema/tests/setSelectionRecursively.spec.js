c05bb0db1dc9228d5dad2d85b54d08e88fce1a9e
Chris Woodward <cwoodward@narrative.io>
2022-01-26 10:44:40 -0800

    Added remaining test for cases with only Objects and Primitives

--- library/src/modules/app/schema/tests/setSelectionRecursively.spec.js
+++ library/src/modules/app/schema/tests/setSelectionRecursively.spec.js
@@ -13,25 +13,77 @@ import {
 	AllAttributes
 } from './testAttributes.js'
 
-let deepCopy = (val) => JSON.parse(JSON.stringify(val))
+let attributes
 
-const attributes = AllAttributes.map(attribute => replacePropertyRefs(attribute, AllAttributes))
+let deepCopy = (val) => JSON.parse(JSON.stringify(val))
 
 let findAttributeById = (attributeId) => {
   return attributes.find(attribute => attribute.id === attributeId)
 }
 
-// let setSelectionRecursively = (property, selectionType, value)
+let log = (arg) => {
+  return JSON.stringify(arg, null, 4)
+}
 
 describe("setSelectionRecursively", function() {
+	beforeEach(function() {
+		attributes = deepCopy(AllAttributes).map(attribute => replacePropertyRefs(attribute, AllAttributes))
+  });
   it("Primitive Attribute", function() {
 		const property = findAttributeById(Primitive.id)
-		const selectionType = 'fliterable'
+		const selectionType = 'filterable'
 		const value = true
 
 		setSelectionRecursively(property, selectionType, value)
-		const target = findAttributeById(Primitive.id).fliterable === true
+		const target = findAttributeById(Primitive.id).filterable === true
 
-    expect(target).toEqual(value)
+		expect(target).toEqual(value)
+	})
+	it("Object -> Primitive, target Object", function() {
+		const property = findAttributeById(ObjectChildPrimitive.id)
+		const selectionType = 'filterable'
+		const value = true
+
+		setSelectionRecursively(property, selectionType, value)
+		const target = 
+			findAttributeById(ObjectChildPrimitive.id).filterable === true && 
+			findAttributeById(ObjectChildPrimitive.id).properties.primitive_property.filterable === true
+		expect(target).toEqual(value)
+	})
+	it("Object -> Primitive, target Primitive", function() {
+		const property = findAttributeById(ObjectChildPrimitive.id).properties.primitive_property
+		const selectionType = 'filterable'
+		const value = true
+
+		setSelectionRecursively(property, selectionType, value)
+		const target = findAttributeById(ObjectChildPrimitive.id).properties.primitive_property.filterable === true
+		expect(target).toEqual(value)
+	})
+	it("Object[1] -> Object[11] -> Primitive, target Object[1]", function() {
+		const property = findAttributeById(ObjectChildObject.id)
+		const selectionType = 'filterable'
+		const value = true
+
+		setSelectionRecursively(property, selectionType, value)
+		const target = 
+			findAttributeById(ObjectChildObject.id).filterable === true &&
+			findAttributeById(ObjectChildObject.id).properties.object.filterable === true && 
+			findAttributeById(ObjectChildObject.id).properties.object.properties.primitive.filterable === true
+			
+		expect(target).toEqual(value)
+	})
+	it("Object[1] -> Object[11] -> Primitive, target Object[2]", function() {
+		const property = findAttributeById(ObjectChildObject.id).properties.object
+		const selectionType = 'filterable'
+		const value = true
+
+		setSelectionRecursively(property, selectionType, value)
+		const target = 
+			findAttributeById(ObjectChildObject.id).properties.object.filterable === true && 
+			findAttributeById(ObjectChildObject.id).properties.object.properties.primitive.filterable === true
+			
+		expect(target).toEqual(value)
 	})
 })
+
+  
