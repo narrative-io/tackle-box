f81446da8cbe465d3d61703c18bfd556e14a6a2b
Chris Woodward <cwoodward@narrative.io>
2022-02-23 17:23:33 -0800

    Added learn more redirect to NioSubscriptionFileDownload

--- library/src/components-private/subscription/SubscriptionFileDownload.vue
+++ library/src/components-private/subscription/SubscriptionFileDownload.vue
@@ -27,7 +27,7 @@
     p.nio-p.text-primary-dark When your first file is ready it will appear here. This might take up to 24 hours.
     NioButton(
       normal-secondary
-      @click="seeHow"
+      @click="learnMore"
     ) Learn about file deliveries
   .loading(
     v-else
@@ -47,11 +47,11 @@ import { getThemeColor } from '@/modules/app/theme/theme'
 import axios from 'axios'
 
 export default {
-	name: 'nio-subscription-file-download',
+  name: 'nio-subscription-file-download',
   components: { NioButton, NioIconFramer, NioSlatTable },
   props: {
-		subscription: { type: Object, required: true },
-		openApiToken: { type: String, required: true },
+    subscription: { type: Object, required: true },
+    openApiToken: { type: String, required: true },
     openApiBaseUrl: { type: String, required: true }
   },
   data: () => ({
@@ -60,25 +60,25 @@ export default {
     preparingDownload: false
   }),
   computed: {
-		primaryColor() {
+    primaryColor() {
       return getThemeColor('primary')
     }
   },
   mounted() {
-		this.reqHeaders = {
-			headers: {
-				'Authorization': `Bearer ${this.openApiToken}`
-			}
-		}
+    this.reqHeaders = {
+      headers: {
+        'Authorization': `Bearer ${this.openApiToken}`
+      }
+    }
     this.getDeliveries()
     this.makeColumns()
   },
   methods: {
     getDeliveries() {
-			axios.get(`${this.openApiBaseUrl}/data-shops/subscriptions/${this.subscription.id}/deliveries`, this.reqHeaders).then(resp => {
-				this.fileDeliveries = resp.data.records
-				this.loading = false
-			})
+      axios.get(`${this.openApiBaseUrl}/data-shops/subscriptions/${this.subscription.id}/deliveries`, this.reqHeaders).then(resp => {
+        this.fileDeliveries = resp.data.records
+        this.loading = false
+      })
     },
     makeColumns() {
       this.columns = [
@@ -95,36 +95,36 @@ export default {
           computed: this.computeExpiration
         }
       ]
-		},
-		
+    },
+    
 // 		const a = '2022-02-08T19:51:00Z'
 
-		// const momentDate = moment.utc(a).format("YYYY-MM-DD")
+    // const momentDate = moment.utc(a).format("YYYY-MM-DD")
 
-		// const timestamp = new Date(a)
-		// const year = timestamp.getUTCFullYear()
-		// let month = timestamp.getUTCMonth() + 1
-		// month = String(month).length === 1 ? `0${month}` : month
-		// let date = timestamp.getUTCDate()
-		// date = String(date).length === 1 ? `0${date}` : date
-		// const js = `${year}-${month}-${date}`
+    // const timestamp = new Date(a)
+    // const year = timestamp.getUTCFullYear()
+    // let month = timestamp.getUTCMonth() + 1
+    // month = String(month).length === 1 ? `0${month}` : month
+    // let date = timestamp.getUTCDate()
+    // date = String(date).length === 1 ? `0${date}` : date
+    // const js = `${year}-${month}-${date}`
 
-		// console.log("moment")
-		// console.log(momentDate)
-		// console.log("js")
-		// console.log(js)
+    // console.log("moment")
+    // console.log(momentDate)
+    // console.log("js")
+    // console.log(js)
     computeFilename(item) {
       if (item.file) {
         return item.file
       }
-			const timestamp = new Date(a)
-			const year = timestamp.getUTCFullYear()
-			let month = timestamp.getUTCMonth() + 1
-			month = String(month).length === 1 ? `0${month}` : month
-			let date = timestamp.getUTCDate()
-			date = String(date).length === 1 ? `0${date}` : date
-			const deliveryDate = `${year}-${month}-${date}`
-						/*
+      const timestamp = new Date(a)
+      const year = timestamp.getUTCFullYear()
+      let month = timestamp.getUTCMonth() + 1
+      month = String(month).length === 1 ? `0${month}` : month
+      let date = timestamp.getUTCDate()
+      date = String(date).length === 1 ? `0${date}` : date
+      const deliveryDate = `${year}-${month}-${date}`
+            /*
        * `transactionBatchId` is a UUID. The first 8 characters of the UUID encode the the lowest 32 bits of the time
        * input to the UUID generation algorithm. It'll be safe for a long time to assume that no two batch ids for the
        * same order will be generated at the same microsecond, and we'd expect to only see collisions after ~2^16
@@ -134,14 +134,14 @@ export default {
       return `${this.subscription.name}-${deliveryDate}-${identifier}.csv`
     },
     computeExpiration(item) {
-			const expiration = new Date(item.timestamp)
-			expiration.setDate(expiration.getDate() + 30)
-			return `${this.dateDiff(expiration, new Date())} days from today`
-		},
-		dateDiff(d2, d1) {
-			let t2 = d2.getTime()
-			let t1 = d1.getTime()
-			return Math.floor((t2-t1)/(24*3600*1000));
+      const expiration = new Date(item.timestamp)
+      expiration.setDate(expiration.getDate() + 30)
+      return `${this.dateDiff(expiration, new Date())} days from today`
+    },
+    dateDiff(d2, d1) {
+      let t2 = d2.getTime()
+      let t1 = d1.getTime()
+      return Math.floor((t2-t1)/(24*3600*1000));
     },
     downloadFile(fileDelivery) {
       this.preparingDownload = true
@@ -152,9 +152,9 @@ export default {
           file: fileDelivery.file,
           file_name_override: this.computeFilename(fileDelivery)
         }
-				axios.post(`${this.openApiBaseUrl}/data-shops/subscriptions/${this.subscription.id}/deliveries/download`, req, this.reqHeaders).then(resp => {
-					this.downloadUrl(resp.data.download_url)
-				})
+        axios.post(`${this.openApiBaseUrl}/data-shops/subscriptions/${this.subscription.id}/deliveries/download`, req, this.reqHeaders).then(resp => {
+          this.downloadUrl(resp.data.download_url)
+        })
         return
       }
     },
@@ -177,10 +177,10 @@ export default {
     },
     delay(ms) {
       return new Promise(resolve => setTimeout(resolve, ms))
-		},
-		seeHow() {
-
-		}
+    },
+    learnMore() {
+      window.open('https://kb.narrative.io/narrative-file-downloads', '_blank')
+    }
   }
 }
 </script>
