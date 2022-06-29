bbdc1cd499f0f751a89da9e9e0e8a00e99260104
Chris Woodward <cwoodward5@hotmail.com>
2022-06-29 16:31:34 -0700

    Created forecast widget, test component and lib exports

---
+++ library/src/components-private/forecasting/ForecastWidget.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+  .nio-forecast-widget
+</template>
+
+<script>
+
+export default {
+  components: {  },
+  name: 'nio-forecast-widget',
+  props: {
+  
+  },
+  computed: {
+    
+  },
+  methods: {
+   
+  }
+};
+</script>
+
+<style lang="sass">
+  @import "../../styles-private/forecasting/_forecast-widget"
+</style>
