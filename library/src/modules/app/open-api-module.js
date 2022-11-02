2f517fdaab3e98b79b06b250fa1f3308dc351c92
Adephil <adebisi.ahmed10@gmail.com>
2022-11-02 18:16:42 +0100

    token expiration check

--- library/src/modules/app/open-api-module.js
+++ library/src/modules/app/open-api-module.js
@@ -2,7 +2,16 @@
 import Vue from 'vue'
 import axios from 'axios'
 
-const httpModule = (config) => {
+
+
+
+let _initCallbackFn = null
+let _initialized = false
+let _token = null
+let _baseURL = null
+let _refreshToken = null
+
+const httpModule = (config, {baseURL, token, refreshToken}) => {
   const http = axios.create(config)
 
   http.interceptors.response.use(
@@ -10,25 +19,40 @@ const httpModule = (config) => {
       return response;
     },
     async (error) => {
-      console.log({error, er: error.response, j: JSON.stringify(error.response)});
-      if (error.response.status === 401) {
-        const payload = {
-          access_token: authData.token,
-          refresh_token: authData.refreshToken,
-          grant_type: refresh_token,
-
-        };
+      console.log({error, er: error.response, j: JSON.stringify(error.response)})
+      const message = error.response.data.error
+      console.log({message})
+      if (error.response.status === 401 && message === "expired_token") {
+        const payload = new FormData();
+        console.log({payload});
+        payload.append("refresh_token", "0M22qEH7U+GuK7ulWIy0Lw==")
+        payload.append("grant_type", "refresh_token")
+        payload.append("client_id", "ec317505e3682dce687ef9aecd39cb72")
+        console.log({payload});
         
         //Handle refresh
-        const response = await axios.post(
-          "/auth/refreshtoken",
-          payload
-        );
-        
-        error.config.headers[
-          "Authorization"
-        ] = `bearer ${response.data.access_token}`;
-        return axios(error.config);
+        try {
+          const response = await axios.post(
+            `${baseURL}/oauth/token`,
+            payload
+          );
+
+          const newToken = response.data.access_token
+          _token = response.data.access_token
+          _refreshToken = response.data.refresh_token
+          console.log({_token, _refreshToken})
+
+          error.config.headers[
+            "Authorization"
+          ] = `bearer ${newToken}`;
+
+          return axios(error.config);
+        } catch (error) {
+          const message = error.response
+          console.log({message})
+          return Promise.reject(error);
+        }
+       
       } else {
         return Promise.reject(error);
       }
@@ -37,13 +61,6 @@ const httpModule = (config) => {
 
   return http;
 }
-
-
-let _initCallbackFn = null
-let _initialized = false
-let _token = null
-let _baseURL = null
-let _refreshToken = null
 
 const openApiModule = {
   initCallback(callbackFn) {
@@ -59,7 +76,7 @@ const openApiModule = {
         'Authorization': `Bearer ${token}`
       }
     }
-    Vue.prototype.$nioOpenApi = httpModule(axiosConfig)
+    Vue.prototype.$nioOpenApi = httpModule(axiosConfig, {baseURL, token, refreshToken})
     _initialized = true
     _token = token
     _baseURL = baseURL
