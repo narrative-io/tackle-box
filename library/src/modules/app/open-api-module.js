698dd4ac35a17ff8916e8271b86c9a27cd64ede7
Adephil <adebisi.ahmed10@gmail.com>
2022-11-07 19:20:07 +0100

    clean up

--- library/src/modules/app/open-api-module.js
+++ library/src/modules/app/open-api-module.js
@@ -16,22 +16,22 @@ const httpModule = (config, {baseURL, token, refreshToken, clientId}) => {
 
   http.interceptors.response.use(
     (response) => {
-      return response;
+      return response
     },
     async (error) => {
       if (error.response.status === 401 && message === "expired_token") {
        
-        const  payload = new URLSearchParams();
-        payload.append('refresh_token', refreshToken);
-        payload.append('grant_type', 'refresh_token');
-        payload.append('client_id', clientId);
+        const  payload = new URLSearchParams()
+        payload.append('refresh_token', refreshToken)
+        payload.append('grant_type', 'refresh_token')
+        payload.append('client_id', clientId)
        
         //Handle refresh
         try {
           const response = await axios.post(
             `${baseURL}/oauth/token`,
             payload
-          );
+          )
 
           const newToken = response.data.access_token
           _token = response.data.access_token
@@ -39,20 +39,20 @@ const httpModule = (config, {baseURL, token, refreshToken, clientId}) => {
 
           error.config.headers[
             "Authorization"
-          ] = `bearer ${newToken}`;
+          ] = `bearer ${newToken}`
 
-          return axios(error.config);
+          return axios(error.config)
         } catch (error) {
-          return Promise.reject(error);
+          return Promise.reject(error)
         }
        
       } else {
-        return Promise.reject(error);
+        return Promise.reject(error)
       }
     }
   )
 
-  return http;
+  return http
 }
 
 const openApiModule = {
