214923c69099861447fc57af760908b50032958f
Chris Woodward <cwoodward5@hotmail.com>
2022-12-06 15:38:13 -0800

    Add forecast widget

--- library/src/components-private/forecasting/ForecastWidget.vue
+++ library/src/components-private/forecasting/ForecastWidget.vue
@@ -1,107 +1,246 @@
 <template lang="pug">
-  .nio-forecast-widget
-    .heading
-      .nio-h3.text-white Volume Estimator {{ state }}
-        NioTooltip.array-join-target(
-          message=""
-          content-class=""
-        )
-      NioPill(
-        text="alpha"
-      )
-    .results
-      .results-text
-        .matched.nio-h4.text-white 3.5 million rows matched*
-        .scanned.nio-h4.text-primary-light 5 million rows scanned
-      .results-graphic
-        v-progress-linear(
-          indeterminate
-          v-if="state === 'running'"
-          :color="progressColor"
-        )
-      .results-annotation.nio-h4.text-primary-light *Estimated matched rows based on applied filters
-    .call-to-action
+  .nio-forecasting-widget
+    .d-flex
+      .title-description.mr-16
+        .filter-title.nio-h4.text-primary-darker Data Forecast 
+          NioPill(
+            text="Alpha"
+            :background-color="getThemeColor('primaryDark')"
+          )
+        .description.nio-p.text-primary-dark Press the Generate Forecast button to scan providers' available datasets that meet your order's criteria and receive an estimate of the quantity of data you will receive.
+      .d-flex.flex-column.full-width  
+        .filter-value-container.full-width
+          .filter-value
+            .observed-data.module
+              .module-content
+                .nio-h6.text-primary-darker Data Scanned
+                .result-value(v-if="costForecastResults === 'loading'")
+                  v-progress-circular.progress(
+                    size="32" 
+                    color="#1438F5"
+                    indeterminate 
+                  )
+                .result-value(v-else-if="costForecastResults === null || costForecastError")
+                  .nio-h3.text-primary-light Not Available
+                .result-value(v-else)
+                  .nio-h3.text-primary-darker {{ rowsScanned }}
+                  .nio-h5.text-primary-light {{ sizeScanned }}
+            .delivered-data.module
+              //- NioDivider(vertical-solo)
+              .module-content
+                .nio-h6.text-primary-darker Deliverable Data
+                .result-value(v-if="forecastResults === 'loading'")
+                  v-progress-circular.progress(
+                    size="32" 
+                    color="#1438F5"
+                    indeterminate 
+                  )
+                .result-value(v-else-if="forecastResults === null || forecastError")
+                  .nio-h3.text-primary-light Not Available
+                .result-value(v-else)
+                  .nio-h3.text-primary-darker {{ deliverableRows }}
+                  .nio-h5.text-primary-light {{ forecastCost }}
+        NioDivider(horizontal-solo)
+        .group-by-forecast
+          NioCheckbox(
+            v-model="enableGrouping"
+            label="Group results by attribute"
+          )
+          .enable-group(v-if="enableGrouping")
+            NioSelect(
+              v-model="groupBy"
+              :items="selectOptions"
+              item-text="label"
+              item-value="value"
+              small
+            )
+            .d-flex.justify-center(v-if="forecastResults === 'loading'")
+              v-progress-circular.progress(
+                size="32" 
+                color="#1438F5"
+                indeterminate 
+              )
+            .data-points(v-if="showResults")
+              .d-flex.justify-space-between.data-point.py-3(v-for="group, i in currentGroup")
+                p.nio-p.text-primary-dark.my-0 {{group.group}}
+                h3.nio-h3.text-primary-dark.my-0 {{formatNumberVerbose(group.rows)}}
+            
+            .d-flex.justify-end.align-center.mb-4.mt-4(v-if="showResults && groupResult.length")
+              .nio-h4.text-primary-darker {{ Math.min(page * perPage, groupResult.length) }} of {{ groupResult.length }}
+              .d-flex
+                NioButton(container @click="goPrevious()")
+                  NioIcon(
+                    name="utility-chevron-left",
+                    :size="16",
+                    color="#AEB9E8",
+                  ) Previous
+                NioButton(container, @click.prevent="goNext()")
+                  NioIcon(name="utility-chevron-right", :size="16", color="#AEB9E8") Next
+
+            NioButton.mt-4(normal-tertiary v-if="groupResult.length" @click="toggleResults = !toggleResults") {{toggleResults ? 'Hide' : 'Show'}} grouped results
+    NioDivider(horizontal-solo)
+    .d-flex.justify-end
       NioButton(
-        :disabled="state === 'running'"
-        normal-secondary
-        @click="runForecast"
-      ) Scan Data
+      normal-secondary
+      :disabled="!forecastParams || forecastResults === 'loading' || costForecastResults === 'loading'"
+      @click="generateForecast"
+    ) Generate Forecast
 </template>
 
 <script>
 
-import NioPill from '@/components/Pill'
-import NioTooltip from '@/components/Tooltip'
-import NioButton from '@/components/Button'
+import numeral from 'numeral'
 import { getThemeColor } from '@/modules/app/theme/theme'
-import axios from 'axios'
-import { getForecast } from '@/modules/app/forecast/forecastModule'
-import { formatNumberVerbose } from '@/modules/helpers'
 
 export default {
-  components: { NioPill, NioTooltip, NioButton },
-  name: 'nio-forecast-widget',
-  props: {
-    openApiToken: { type: String, required: true },
-    openApiBaseUrl: { type: String, required: true }
+  mounted() {
+    this.updateDefaultOption();
   },
-  data: () => ({
-    state: 'initial',
-    reqHeaders: null,
-    forecastResult: null,
-    costForecastResult: null
-  }),
-  computed: {
-    progressColor() {
-      return getThemeColor('primaryDark')
+  data() {
+    return {
+      enableGrouping: false,
+      groupBy: '',
+      groupResult: [],
+      toggleResults: true,
+      page: 0,
+      perPage: 5,
+      currentGroup: [],
+    }
+  },
+  props: {
+    forecastParams: { type: Object, required: false },
+    forecastResults: { type: String | Object, required: true},
+    costForecastResults: { type: String | Object, required: true},
+    forecastStale: { type: Boolean, required: false, default: false },
+    filters: {type: Array, default: () => []}
+  },
+  watch: {
+    forecastResults(val) {
+      let result = (val?.result?.datasets || []).filter(({group}) => group)
+      let groupResult = result.reduce((acc, result)  => {
+        const { group } = result
+        acc[group] =  acc[group] ? { ...acc[group], rows: acc[group].rows + result.rows } : result
+        return acc
+      }, {})
+      this.groupResult = Object.values(groupResult)
+      this.goNext()
     },
-    rowsScannedMessage() {
-      if (this.costForecastResults && this.costForecastResults.result.cost.rows === 0) {
+    groupBy(prev, val) {
+      if(prev === val) return
+      this.resetState()
+    }
+  },
+  computed: {
+    showResults() {
+      return this.forecastResults !== 'loading' && this.toggleResults
+    },
+    selectOptions() {
+      const result = this.filters.filter(filter => filter.value === "ifPresent").map(({path}) =>  {
+        const [title, value = ''] = path
+        const optionValue = `${title?.name}${value ? `.${value}` : ''}`
+        return {label: title.displayName, value: optionValue}
+      })
+      return result
+    },
+    rowsScanned() {
+      if (this.costForecastResults.result?.cost?.rows === 0) {
         return "No matching rows"
       }
-      return `${formatNumberVerbose(this.costForecastResults.result.cost.rows)} rows scanned`
+      return this.formatNumberVerbose(this.costForecastResults.result.cost.rows)
     },
-    deliverableRowsMessage() {
+    sizeScanned() {
+      if (this.costForecastResults.result?.cost?.size === 0) {
+        return ""
+      }
+      return this.readableSize(this.costForecastResults.result.cost.size)
+    },
+    deliverableRows() {
       if (this.forecastResults.result.rows === 0) {
         return "No matching rows"
       }
-      return `${formatNumberVerbose(this.forecastResults.result.rows)} rows matched`
-    }
-  },
-  mounted() {
-    this.reqHeaders = {
-      headers: {
-        'Authorization': `Bearer ${this.openApiToken}`
+      return this.formatNumberVerbose(this.forecastResults.result.rows)
+    },
+    forecastCost() {
+      if (this.forecastResults.result.rows === 0) {
+        return ""
       }
+      return this.formatCurrencyNoCents(this.forecastResults.result.cost / 100000000) + ' Estimated Data Cost'
+    },
+    forecastError() {
+      return !this.forecastResults || 
+        !this.forecastResults || 
+        this.forecastResults.state !== 'completed'
+    },
+    costForecastError() {
+      return !this.costForecastResults || 
+        this.costForecastResults.state !== 'completed'
     }
   },
   methods: {
-    runForecast() {
-      this.state = 'running'
-      this.fetchForecast()
+    resetState() {
+      this.currentGroup = []
+      this.groupResult = []
+      this.page = 0
+      this.perPage = 5
     },
-    fetchForecast() {
-      setTimeout(() => {
-        this.forecastResult = null
-        this.costForecastResults = null
-        this.state = 'success'
-      }, 2000)
-      getForecast(this.forecastParams, 'forecasts', this.openApiBaseUrl, this.openApiToken).then(res => {
-        this.forecastResults = res
-        thie.$emit('forecastUpdated', res.data)
-      })
-      getForecast(this.forecastParams, 'cost-forecasts', this.openApiBaseUrl, this.openApiToken).then(res => {
-        this.costForecastResults = res
-        thie.$emit('costForecastUpdated', res.data)
-      })
+    updateDefaultOption() {
+      if(!this.selectOptions?.length) return
+      const [option] = this.selectOptions
+      this.groupBy = option.value
     },
-    forecastCompleted() {
-      this.state = 'completed'
+    generateForecast() {
+      this.$emit('generateForecast', {enableGrouping: this.enableGrouping, groupBy: this.groupBy})
+    },
+    formatNumber(number) {
+      if (isNaN(number))
+        return 0
+      if (number) {
+        if (Math.abs(number) > 1000)
+          return numeral(number).format('0.0a')
+        else 
+        return numeral(number).format('0,0')
+      }
+      else return number
+    },
+    formatNumberVerbose(number) {
+      if (number > 0) {
+        return this.formatNumber(number).replace('m', ' Million').replace('b', ' Billion').replace('b', ' Billion').replace('t', ' Trillion') + ' Rows'
+      }
+    },
+    readableSize(number) {
+      var i = Math.floor( Math.log(number) / Math.log(1024) )
+      return ( number / Math.pow(1024, i) ).toFixed(0) * 1 + ' ' + ['B', 'KB', 'MB', 'GB', 'TB'][i]
+    },
+    formatCurrencyNoCents(number) {
+      if (Math.abs(number) > 1000)
+        return numeral(number).format('$0,0')
+      else
+        return numeral(number).format('$0')
+    },
+    getThemeColor(colorName) {
+      return getThemeColor(colorName)
+    },
+    goPrevious() {
+      if (this.page === 1) return
+      this.page -= 1
+      this.currentGroup = [...this.paginate(this.groupResult, this.page, this.perPage)]
+    },
+    paginate(data = [], page, perPage) {
+      const start = perPage * (page - 1)
+      const end = perPage * page
+      return data.slice(start, end)
+    },
+    goNext() {
+      if (this.page * this.perPage >= this.groupResult.length) {
+        return
+      }
+      this.page += 1
+      this.currentGroup = [...this.paginate(this.groupResult, this.page, this.perPage)]
     }
   }
 };
 </script>
 
-<style lang="sass">
+<style lang="sass" scoped>
   @import "../../styles-private/forecasting/_forecast-widget"
 </style>
